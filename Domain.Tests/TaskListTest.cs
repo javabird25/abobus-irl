4da856f8efd1161824a34922ca26b873992686e4
javabird25 <shockck84@gmail.com>
2022-10-19 19:27:25 +0500

    TaskList tests

---
+++ Domain.Tests/TaskListTest.cs
@@ -0,0 +1,34 @@
+namespace Abobus.Domain.Tests;
+
+using Abobus.Domain;
+using Xunit;
+using FluentAssertions;
+using System.Collections.Generic;
+
+public class TaskListTest
+{
+    [Fact]
+    public void Tasks_GivenTaskList_ReturnsItsTasks()
+    {
+        var task = new Task(new Puzzle(1, "Puzzle desc", new ImageOffset(0, 0), "Good job"));
+        var taskList = new TaskList(new List<Task> { task });
+        taskList.Tasks[0].Should().Be(task);
+    }
+
+    [Fact]
+    public void PercentCompleted()
+    {
+        var task1 = new Task(new Puzzle(1, "Puzzle desc", new ImageOffset(0, 0), "Good job"));
+        var task2 = new Task(new Puzzle(2, "Puzzle desc", new ImageOffset(0, 0), "Good job"));
+        var task3 = new Task(new Puzzle(3, "Puzzle desc", new ImageOffset(0, 0), "Good job"));
+        var taskList = new TaskList(new List<Task> { task1, task2, task3 });
+
+        taskList.PercentCompleted.Should().Be(0);
+        task1.Complete();
+        taskList.PercentCompleted.Should().Be(33);
+        task2.Complete();
+        taskList.PercentCompleted.Should().Be(66);
+        task3.Complete();
+        taskList.PercentCompleted.Should().Be(100);
+    }
+}
--- Domain/TaskList.cs
+++ Domain/TaskList.cs
@@ -3,6 +3,7 @@ namespace Abobus.Domain;
 public sealed class TaskList
 {
     public List<Task> Tasks { get; init; }
+    public int PercentCompleted => Tasks.Where(task => task.IsCompleted).Count() * 100 / Tasks.Count;
 
     public TaskList(List<Task> tasks)
     {
