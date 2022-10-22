55b70604aba7aa3e8fbd20b470cd4510cd5d0266
javabird25 <shockck84@gmail.com>
2022-10-22 14:18:52 +0500

    Remove "public" from interfaces

--- Application/IGameRepository.cs
+++ Application/IGameRepository.cs
@@ -4,7 +4,7 @@ using Domain;
 
 public interface IGameRepository
 {
-    public (IGame, GamePassword) CreateGame(string mapName, string configName);
-    public IGame GetGameByPassword(GamePassword password);
-    public void UpdateGame(IGame game);
+    (IGame, GamePassword) CreateGame(string mapName, string configName);
+    IGame GetGameByPassword(GamePassword password);
+    void UpdateGame(IGame game);
 }
--- Domain/IGameConfig.cs
+++ Domain/IGameConfig.cs
@@ -2,7 +2,7 @@ namespace Abobus.Domain;
 
 public interface IGameConfig
 {
-    public string Name { get; }
+    string Name { get; }
 
-    public T Get<T>(string key, T fallback) where T : notnull;
+    T Get<T>(string key, T fallback) where T : notnull;
 }
